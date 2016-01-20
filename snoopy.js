e5afe8ac4efc5a2a557d221f81e2ccb71c06d85b
Daniel Hug <daniel@hugwebdesign.com>
2016-01-20 14:33:09 -0800

    update README

--- snoopy.js
+++ snoopy.js
@@ -1,39 +1,41 @@
 var Snoopy = (function() {
 	function Snoopy(obj) {
+		if (!(this instanceof Snoopy)) return new Snoopy(obj);
+		// don't overwrite this!:
 		this.snoopers = {};
 		Object.keys(obj).forEach(function(key) {
 			this[key] = obj[key];
 		}, this);
 	}
 
-	Snoopy.prototype.changed = function(prop) {
+	var changed = Snoopy.prototype.changed = function(prop) {
 		var newVal = this[prop];
 		(this.snoopers[prop] || []).forEach(function(snooper) {
-			snooper(newVal);
+			snooper.call(this, newVal);
 		});
 	};
 
-	Snoopy.prototype.get = function(prop) {
+	var get = Snoopy.prototype.get = function(prop) {
 		return this[prop];
 	};
 
 	Snoopy.prototype.set = function(prop, val) {
 		this[prop] = val;
-		this.changed(prop);
+		changed.call(this, prop);
 	};
 
 	Snoopy.prototype.snoop = function(props, snooper) {
 		if (snooper) {
 			var propsArr = props.split(' ');
-			
+
 			// caller calls snooper with the values of the passed props
-			var propToVal = this.get.bind(this);
+			var propToVal = get.bind(this);
 			var caller = function caller() {
 				snooper.apply(null, propsArr.map(propToVal));
 			};
-			
+
 			caller();
-			
+
 			// push caller to snooper list
 			var t = this;
 			propsArr.forEach(function(prop) {
